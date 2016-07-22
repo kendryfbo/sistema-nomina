2d933c54a356ce17615f02c9deefc7c71f089e5e
kendryfbo <kendryfbo@gmail.com>
2016-07-22 11:09:48 -0400

    Se agregaron las constantes de tablas y se definieron diversas funciones
    - Se agregaron as constantes de tabla
    - Se definieron Diversas Funciones
    - Se agrego la Funcion 'findVacacionCargada(vacacionNum)' que devuelve
    un objeto vacacion.
    - Se eliminaron Varias Funciones que no se implementaran por los
    momentos.

--- vacacionmodel.h
+++ vacacionmodel.h
@@ -18,7 +18,11 @@ public:
     ~VacacionModel();
 
     // Generation of all employees vacations in selected area from vacation
-    generateVacacion(Vacacion vacacion,int diasBono,int diasAjuste,QStringList codDeducciones); // falta por implementar
+    bool generateVacacion(Vacacion vacacion,int diasBono,int diasAjuste); // falta por implementar
+
+    // Insert deducciones a procesar en vacacion Generada
+    bool insertDeduccionEnVacacion(QString deduccionCod,int cantidad);
+
 
     // Process of all employees vacations from the corresponding vacacion cargada num
     processVacacion(int vacacionNum); // falta por implementar
@@ -26,27 +30,33 @@ public:
 
 private:
 
+    QStringList findEmpeladosFromArea(QString areaCod);
+
     // Insert, Update and Delete from table 'vacacioncargada'
-    insertVacacionCargada(Vacacion vacacion); // falta por implementar
-    updateVacacionCargada(Vacacion vacacion); // falta por implementar
-    deleteVacacionCargada(int vacacionNum); // falta por implementar
+    bool insertVacacionCargada(Vacacion vacacion);
+    bool deleteVacacionCargada(int vacacionNum);
+
+    Vacacion findVacacionCargada(int VacacionNum);
+
+    // Procesar todos los empleados mediante el area de las vacaciones
+    bool insertVacacionCargadaDetalle(Vacacion vacacion,int diasBono,int diasAjuste); // falta por implementar
 
     // Insert, Update and Delete from table 'vacaciondetalle'
-    insertVacacionDetalleCargada(int vacacionNum,QString empleadoCed,int diasBono,int diasAjuste); // falta por implementar
-    updateVacacionDetalleCargada(int vacacionNum,QString empleadoCed,int diasBono,int diasAjuste); // falta por implementar
+    bool insertVacacionCargadaDetalleEmp(int vacacionNum,QString empleadoCed,int diasBono,int diasAjuste); // falta por implementar
+
+
     /* Para futuras mejoras en donde se puedan eliminar empleados de las vacaciones cargadas
-    * deleteVacacionDetalleCargada();
-    */
+     * deleteVacacionDetalleCargada();
+     */
 
     // Insert, Update and Delete from table 'vacaciondetallededuccion'
-    insertdetalleDeduccionCargada(); // falta por implementar
-    updatedetalleDeduccionCargada(); // falta por implementar
-    deletedetalleDeduccionCargada();  // falta por implementar
+    insertDeduccionCargadaDetalle(); // falta por implementar
+    updateDeduccionCargadaDetalle(); // falta por implementar
+    deleteDeduccionCargadaDetalle(); // falta por implementar
 
     // Obtener Montos de bono Vacacional y Ajuste Salarial
     double getBonoVacacional(int vacacionNum,QString empleadoCed); // falta por implementar
     double getajusteSalario(int vacacionNum,QString empleadoCed); // falta por implementar
-
     double getBonoIntegral(int vacacionNum,QString empleadoCed);
 };
 
